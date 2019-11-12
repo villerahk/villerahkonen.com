5b0f6b299b514fa082cd14a8890e7f23d00b8f15
villerahk <ville.rahkonen10@gmail.com>
2019-11-12 21:43:25 +0200

    Minor fixes and adjustments.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import LinearProgress from '@material-ui/core/LinearProgress';
